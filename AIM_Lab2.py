4f142df1d3cf9bb8e0d800691f30e43264fbcdf2
KOMMyHuCT <KOMMyHuCT@users.noreply.github.com>
2022-12-12 12:10:28 +0400

    Update

--- AIM_Lab2.py
+++ AIM_Lab2.py
@@ -8,60 +8,74 @@ try:
     K = int(input())
     print("N = ", end = '')
     N = int(input())
-    if (N % 2 | N < 1):
-        raise Exception("Uneven dimensions")
+    if (N < 1):
+        raise Exception("N must me larger than 1")
 
     A = numpy.array([[randint(-10, 10) for j in range(N)] for i in range (N)])
     #A = numpy.array([[0, 1, 2, 3],
     #                [4, 5, 6, 7],
     #                [8, 9, 10, 11],
     #                [12, 13, 14, 15]])
+    #A = numpy.array([[-6, -4,  5],
+    #                 [0, 5, -3],
+    #                 [-5, -3, -8]])
     print(A)
 
-    E = A[0: N // 2, 0: N // 2]
+    remainder = int(N / 2)
+    large = N - remainder
+
+    if (N % 2 != 0):
+        E = A[0: large, 0: large]
+        B = A[0: large, remainder:]
+        D = A[remainder:, 0: large]
+        C = A[remainder:, remainder:]
+    else:
+        E = A[0: N // 2, 0: N // 2]
+        B = A[0: N // 2, N // 2:]
+        D = A[N // 2:, 0: N // 2]
+        C = A[N // 2:, N // 2:]
+
     print("E: ")
     print(E)
 
-    B = A[0: N // 2, N // 2:]
     print("B: ")
     print(B)
 
-    D = A[N // 2:, 0: N // 2]
     print("D: ")
     print(D)
 
-    C = A[N // 2:, N // 2:]
     print("C: ")
     print(C)
-
-    F = A.copy()
 
     zeroesInUnevenColumnsOfE = numpy.count_nonzero(E[:, ::2] == 0)
     print("Zeroes in uneven columns of E: ")
     print(zeroesInUnevenColumnsOfE)
 
-    negativesInEvenRowsOfE = numpy.sum(E[1::2, :] <= 0)
+    negativesInEvenRowsOfE = (E[1::2, :] < 0).sum()
     print("Negatives in even rows of E: ")
     print(negativesInEvenRowsOfE)
 
+    F = A.copy()
+
     if (zeroesInUnevenColumnsOfE > negativesInEvenRowsOfE):
-        B = numpy.flipud(A[N // 2:, N // 2:])
-        C = numpy.flipud(A[0: N // 2, N // 2:])
+        print("More zeroes in uneven columns of E, symmetrically switching C and B")
+        if (N % 2 != 0):
+            B_temp = numpy.flip(B, axis = 0)
+            C_temp = numpy.flip(C, axis = 0)
+            F[remainder:, remainder:] = B_temp
+            F[0: large, remainder:] = C_temp
+        else:
+            B = numpy.flipud(A[N // 2:, N // 2:])
+            C = numpy.flipud(A[0: N // 2, N // 2:])
     else:
-        B = A[0: N // 2, 0: N // 2]
-        E = A[0: N // 2, N // 2:]
+        print("More negatives in even rows of E, non-symmetrically switching B and E")
+        if (N % 2 != 0):
+            F[0: large, 0: large] = B
+            F[0: large, remainder:] = E
+        else:
+            B = A[0: N // 2, 0: N // 2]
+            E = A[0: N // 2, N // 2:]
 
-    print("New matrices: ")
-    print("E: ")
-    print(E)
-    print("B: ")
-    print(B)
-    print("D: ")
-    print(D)
-    print("C: ")
-    print(C)
-
-    F = numpy.concatenate((numpy.concatenate((E, D), axis=0), numpy.concatenate((B, C), axis=0)), axis=1);
     print("F: ")
     print(F)
 
